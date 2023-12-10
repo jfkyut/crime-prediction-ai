1ad34f5de74c9128a9cca6ef403b249debe23fed
Jake Francis Arnosa <jfarnosa.github@gmail.com>
2023-12-10 22:44:48 +0800

    almost done

--- resources/js/Layouts/Custom/AuthLayout.jsx
+++ resources/js/Layouts/Custom/AuthLayout.jsx
@@ -4,13 +4,16 @@ const AuthLayout = ({ user, children, header }) => {
     return (
         <>
             <SideBar user={user} />
-            <div className="bg-gray-800 h-14 sticky top-0 sm:hidden"></div>
             <div className="sm:ml-64 relative dark:bg-gray-900 min-h-screen text-gray-600 dark:text-gray-300 px-[5%] py-24">
+                <div className="bg-gray-800 h-14 fixed top-0 left-0 border-b border-gray-600 z-20 right-0 sm:hidden">
+
+                </div>
+
                 <header className="mb-10">
                     {header}
                 </header>
                 <main>
-                    <div className="fixed right-[5%] top-2">
+                    <div className="fixed right-[5%] top-2 z-30">
                         <div className="text-gray-700 dark:text-gray-300 border dark:border-gray-700 px-4 py-2 rounded-md max-w-[120px] sm:max-w-[150px] md:max-w-[250px] lg:max-w-[300px] truncate">{user.email}</div>
                     </div>
                     {children}
