fa30f64b636a1ff08d9402465b30cfefb98ef9f7
Jake Francis Arnosa <jfarnosa.github@gmail.com>
2023-12-10 22:27:26 +0800

    added sidebar close butotn

--- resources/js/Components/Custom/SideBar.jsx
+++ resources/js/Components/Custom/SideBar.jsx
@@ -63,8 +63,14 @@ const SideBar = ({ user }) => {
             <aside
                 ref={sidebarRef}
                 id="sidebar-multi-level-sidebar"
-                className={`${!openSideBar && "-translate-x-full"} fixed top-0 left-0 z-40 w-64 h-screen transition-transform sm:translate-x-0`}
+                className={`${!openSideBar && "-translate-x-full"} fixed top-0 border-r dark:border-gray-600 left-0 z-40 w-64 h-screen transition-transform sm:translate-x-0`}
                 aria-label="Sidebar">
+                <button onClick={() => setOpenSideBar(false)} type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="sm:hidden text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 end-2.5 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white" >
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                    <span class="sr-only">Close menu</span>
+                </button>
 
                 <div className="h-full px-3 py-4 overflow-y-auto bg-gray-100 dark:bg-gray-800">
                     <div className="p-2 mb-5">
