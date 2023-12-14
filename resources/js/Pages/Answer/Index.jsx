872b38e23b2d5c7dc2d5f59dcdc66886fdbb8ec1
Jake Francis Arnosa <jfarnosa.github@gmail.com>
2023-12-14 10:08:03 +0800

    change remaining zinc to gray

--- resources/js/Pages/Answer/Index.jsx
+++ resources/js/Pages/Answer/Index.jsx
@@ -17,7 +17,7 @@ const Index = ({ auth, answers }) => {
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                     {answers.data.map((answer, index) => (
                         <div key={index} className="w-full flex justify-center items-center">
-                            <div className="bg-gray-50 dark:bg-zinc-800 p-4 rounded-lg h-[150px] w-[300px] border dark:border-zinc-700">
+                            <div className="bg-gray-50 dark:bg-gray-800 p-4 rounded-lg h-[150px] w-[300px] border dark:border-gray-700">
                                 <p className="break-words overflow-hidden h-[70%]">{answer.description}</p>
                                 <div className="h-[30%] flex items-end">
                                     <Link href={route('answer.show', answer.id)} className="text-white bg-lime-700 hover:bg-lime-800 focus:ring-4 focus:ring-lime-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-lime-600 dark:hover:bg-lime-700 dark:focus:ring-lime-800 inline-flex items-center">
--- resources/js/Pages/Answer/Show.jsx
+++ resources/js/Pages/Answer/Show.jsx
@@ -13,8 +13,8 @@ const Show = ({ auth, answer }) => {
             <Head title={answer.description} />
 
             <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table className="w-full text-sm text-left rtl:text-right text-zinc-500 dark:text-zinc-400">
-                    <thead className="text-xs text-zinc-700 uppercase bg-zinc-50 dark:bg-zinc-700 dark:text-zinc-400">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <th scope="col" className="px-6 py-3 w-[30%]">
                                 Category
@@ -25,16 +25,16 @@ const Show = ({ auth, answer }) => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-700 hover:bg-zinc-50 dark:hover:bg-zinc-600">
-                            <th scope="row" className="px-6 py-4 font-medium text-zinc-900 whitespace-nowrap dark:text-white">
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 Situation
                             </th>
                             <td className="px-6 py-4">
                                 {answer.situation}
                             </td>
                         </tr>
-                        <tr className="bg-white border-b dark:bg-zinc-800 dark:border-zinc-700 hover:bg-zinc-50 dark:hover:bg-zinc-600">
-                            <th scope="row" className="px-6 py-4 font-medium text-zinc-900 whitespace-nowrap dark:text-white">
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
                                 Response
                             </th>
                             <td className="px-6 py-4">
