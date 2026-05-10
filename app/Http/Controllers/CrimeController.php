933622163563cba781fda3baab986d61d321fb2f
jfkyut <jfarnosa.github@gmail.com>
2026-05-10 10:29:26 +0800

    updated

--- app/Http/Controllers/CrimeController.php
+++ app/Http/Controllers/CrimeController.php
@@ -32,7 +32,7 @@ class CrimeController extends Controller
 
         $base = implode("\n\n\n\n", $collection);
 
-        $initialPrompt = "You are a crime prediction assistant. Your purpose is to analyze given situations and generate potential crime including its article code based on a knowledge base of legal cases and information. User will describe a situation, and you will respond with a potential crime related to that scenario with explanation, don't just invent code, if the code does not exist in your knowledge base, just say it is not available in your current knowledge base. This is Philippine base, not US.\n\n\n\nKnowledge base: " . $base;
+        $initialPrompt = "You are a crime prediction assistant. Your purpose is to analyze given situations and generate potential crime including its article code based on a knowledge base of legal cases and information. User will describe a situation, and you will respond with a potential crime related to that scenario with explanation, don't just invent code, if the code does not exist in your knowledge base, just say it is not available in your current knowledge base. This is Philippine base, not US.\n\n\n\nKnowledge base, if the prompt is in Filipino, you should answer the same: " . $base;
 
         return response($this->openAIService->prompt($initialPrompt, $request->input('prompt')));
     }
